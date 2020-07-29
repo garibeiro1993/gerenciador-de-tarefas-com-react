ed8c3cc8165a0e5476fa4414e4fb992815d168e4
Gabriel Ribeiro <garibeiro@outlook.com>
2020-07-29 08:30:23 -0300

    Implement update feature

--- src/atualizar/atualizar-tarefa.js
+++ src/atualizar/atualizar-tarefa.js
@@ -1,20 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Button, Form, Jumbotron, Modal } from 'react-bootstrap'
 import { navigate, A } from 'hookrouter'
 
-const AtualizarTarefa = () => {
+const AtualizarTarefa = props => {
+
+  const [exibirModal, setExibirModal] = useState(false)
+  const [formValidado, setFormValidado] = useState(false)
+  const [tarefa, setTarefa] = useState('')
+  const [carregarTarefa, setCarregarTarefa] = useState(true)
+
+  useEffect(() => {
+    if (carregarTarefa) {
+      const tarefasDb = localStorage['tarefas']
+      const tarefas = tarefasDb ? JSON.parse(tarefasDb) : []
+      const tarefa = tarefas.filter(
+        t => t.id === parseInt(props.id)
+      )[0]
+      setTarefa(tarefa.nome)
+      setCarregarTarefa(false)
+    }
+
+  }, [carregarTarefa, props])
 
   const voltar = event => {
     event.preventDefault()
     navigate('/')
   }
 
+  const handleFecharModal = () => navigate('/')
+
+  const atualizar = event => {
+    event.preventDefault()
+    setFormValidado(true)
+    if (event.currentTarget.checkValidity() === true) {
+      //obtem as tarefas
+      const tarefasDb = localStorage['tarefas']
+      let tarefas = tarefasDb ? JSON.parse(tarefasDb) : []
+      // persistir a tarefa atualizada
+      tarefas = tarefas.map(tarefaObj => {
+        if(tarefaObj.id === parseInt(props.id)) {
+          tarefaObj.nome = tarefa
+        }
+        return tarefaObj
+      })
+      localStorage['tarefas'] = JSON.stringify(tarefas)
+      setExibirModal(true)
+    }
+  }
+
+  const handleTxtTarefa = event => setTarefa(event.target.value)
+
     return (
       <div>
         <h3 className="text-center">Atualizar</h3>
         <Jumbotron>
-          <Form noValidate>
+          <Form onSubmit={atualizar} noValidate validated={formValidado}>
             <Form.Group>
               <Form.Label>Tarefa</Form.Label>
               <Form.Control
@@ -23,7 +64,9 @@ const AtualizarTarefa = () => {
                 minLength='5'
                 maxLength='100'
                 required
-                data-testid='txt-tarefa' />
+                data-testid='txt-tarefa'
+                value={tarefa}
+                onChange={handleTxtTarefa} />
               <Form.Control.Feedback type='invalid'>
                 A tarefa deve conter ao menos 5 caracteres
               </Form.Control.Feedback>
@@ -38,6 +81,19 @@ const AtualizarTarefa = () => {
               </A>
             </Form.Group>
           </Form>
+          <Modal show={exibirModal} onHide={handleFecharModal} data-testid="modal">
+            <Modal.Header closeButton>
+              <Modal.Title>Sucesso</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Tarefa atualizada com sucesso!
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="success" onClick={handleFecharModal}>
+                Continuar
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </Jumbotron>
       </div>
     )
